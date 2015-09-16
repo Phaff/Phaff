9eab5cc438872405b9470ab50c6c5b01eb63cb83
Gaël Demette <demette.gael@gmail.com>
2015-09-16 15:13:21 +0200

    Poc for object

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 "use strict";
 
-const ffi = require('ffi');
-const path = require('path');
+const ffi = require("ffi");
+const path = require("path");
+const assert = require("assert");
+const ref = require("ref");
+const StructType = require("ref-struct");
 
-module.exports = ffi.Library(path.join(__dirname, 'target/release/libphaff.dylib'), {
-    hello_world: ['string', []]
+// Define a struct type with properties matching the order and type of the
+// receiving struct type.
+var Args = StructType({
+    init: ref.types.uint32,
+    by: ref.types.uint32
 });
+
+var lib = ffi.Library(path.join(__dirname, 'target/release/libphaff.dylib'), {
+    "createCounter": [ "pointer", [ Args ] ],
+    "destroyCounter": [ "void", [ "pointer" ] ],
+    "getCounterValue": [ "uint32", [ "pointer" ] ],
+    "incrementCounter": [ "uint32", [ "pointer" ] ],
+    "decrementCounter": [ "uint32", [ "pointer" ] ]
+});
+
+class Counter {
+    constructor(value) {
+
+        var args =  new Args({init:value, by: 1});
+        this.ptr = lib.createCounter(args);
+    }
+
+    get() {
+        return lib.getCounterValue(this.ptr);
+    }
+
+    increment() {
+        return lib.incrementCounter(this.ptr);
+    }
+
+    destroy() {
+        lib.destroyCounter(this.ptr);
+    }
+}
+var exports = module.exports = Counter;
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,30 @@
+#![allow(non_snake_case)]
+
+mod counter;
+
+use counter::{Counter, Args};
+use std::mem::transmute;
+
 #[no_mangle]
-pub extern fn hello_world() -> &'static str {
-    return "Hello world";
+pub extern fn createCounter(args: Args) -> *mut Counter {
+let _counter = unsafe { transmute(Box::new(Counter::new(args))) };
+_counter
+}
+
+#[no_mangle]
+pub extern fn getCounterValue(ptr: *mut Counter) -> u32 {
+let mut _counter = unsafe { &mut *ptr };
+_counter.get()
+}
+
+#[no_mangle]
+pub extern fn incrementCounter(ptr: *mut Counter) -> u32 {
+let mut _counter = unsafe { &mut *ptr };
+_counter.incr()
+}
+
+#[no_mangle]
+pub extern fn destroyCounter(ptr: *mut Counter) {
+let _counter: Box<Counter> = unsafe{ transmute(ptr) };
+// Drop
 }
